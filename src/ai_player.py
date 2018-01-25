1555462c4748ccc96c6c225bd32ded9a5214e935
Sean <marzecsean@gmail.com>
2018-01-24 22:06:58 -0500

    added a basic ai pattern beyond random. ai will make one of the moves that flips the most enemy pieces.

--- src/ai_player.py
+++ src/ai_player.py
@@ -3,7 +3,7 @@ from random import choice
 
 class AIPlayer(Player):
 
-    DELAY = 3
+    DELAY = 25
 
     def __init__(self, state, color):
 
@@ -24,10 +24,24 @@ class AIPlayer(Player):
 
     def take_turn(self):
 
-        moves = self.state.game_logic.valid_moves.keys()
-
-        move = choice(moves)
+        move = self.choose_available_move()
         self.play_piece(move)
         self.trigger_flank(move)
 
         self.state.turn_manager.end_turn()
+
+    def choose_available_move(self):
+
+        weighted_moves = {}
+
+        self.weigh_moves(self.state.game_logic.valid_moves, weighted_moves)
+
+        best_value = max(weighted_moves.values())
+        best_moves = filter(lambda x: weighted_moves[x] == best_value, weighted_moves)
+
+        return choice(best_moves)
+
+    def weigh_moves(self, valid_moves, weighted_moves):
+
+        for move in valid_moves:
+            weighted_moves[move] = len(valid_moves[move])
